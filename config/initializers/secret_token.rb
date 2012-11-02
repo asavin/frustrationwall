4811d0c4a33be91b855daefb8fc8c6ead0eb3989
Alex Savin <karismafilms@gmail.com>
2012-11-02 10:59:18 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frustrationwall::Application.config.secret_token = '1fb6c3cf186eb58c9df57999c593ec77d8407f19bfa5c778514d4c9cfceabed0c89b4e6047d7b12c97b4ecd4827b533ebbbd75947c6b577771b37c2140381c00'
